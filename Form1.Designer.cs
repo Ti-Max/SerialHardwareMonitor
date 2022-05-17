39773a46eb04eff7f787b041786af5284babff55
Ti-Max <maxtimofy@gmail.com>
2022-05-17 23:22:52 +0200

    First project upload

---
+++ Form1.Designer.cs
@@ -0,0 +1,232 @@
+﻿namespace SerialHardwareMonitor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.choosedStats = new System.Windows.Forms.CheckedListBox();
+            this.Start = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.serialPorts = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.interval = new System.Windows.Forms.NumericUpDown();
+            this.Stop = new System.Windows.Forms.Button();
+            this.timerSerial = new System.Windows.Forms.Timer(this.components);
+            this.baudRate = new System.Windows.Forms.ComboBox();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.label4 = new System.Windows.Forms.Label();
+            this.rawData = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.interval)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // choosedStats
+            // 
+            this.choosedStats.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.choosedStats.CheckOnClick = true;
+            this.choosedStats.FormattingEnabled = true;
+            this.choosedStats.Location = new System.Drawing.Point(12, 12);
+            this.choosedStats.Name = "choosedStats";
+            this.choosedStats.Size = new System.Drawing.Size(120, 122);
+            this.choosedStats.TabIndex = 0;
+            // 
+            // Start
+            // 
+            this.Start.Location = new System.Drawing.Point(144, 130);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(54, 23);
+            this.Start.TabIndex = 1;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = true;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(138, 12);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(54, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Serial port";
+            // 
+            // serialPorts
+            // 
+            this.serialPorts.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.serialPorts.FormattingEnabled = true;
+            this.serialPorts.Location = new System.Drawing.Point(138, 28);
+            this.serialPorts.Name = "serialPorts";
+            this.serialPorts.Size = new System.Drawing.Size(121, 21);
+            this.serialPorts.TabIndex = 5;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(138, 52);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Baud rate";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(138, 90);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Interval mls";
+            // 
+            // interval
+            // 
+            this.interval.Increment = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            this.interval.Location = new System.Drawing.Point(138, 106);
+            this.interval.Maximum = new decimal(new int[] {
+            1000000,
+            0,
+            0,
+            0});
+            this.interval.Name = "interval";
+            this.interval.Size = new System.Drawing.Size(120, 20);
+            this.interval.TabIndex = 7;
+            this.interval.Value = new decimal(new int[] {
+            1000,
+            0,
+            0,
+            0});
+            // 
+            // Stop
+            // 
+            this.Stop.Enabled = false;
+            this.Stop.Location = new System.Drawing.Point(203, 130);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(54, 23);
+            this.Stop.TabIndex = 9;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = true;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // timerSerial
+            // 
+            this.timerSerial.Interval = 1000;
+            this.timerSerial.Tick += new System.EventHandler(this.timerSerial_Tick);
+            // 
+            // baudRate
+            // 
+            this.baudRate.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.baudRate.FormattingEnabled = true;
+            this.baudRate.Items.AddRange(new object[] {
+            "1200",
+            "2400",
+            "4800",
+            "9600",
+            "19200",
+            "38400",
+            "57600",
+            "115200"});
+            this.baudRate.Location = new System.Drawing.Point(138, 68);
+            this.baudRate.Name = "baudRate";
+            this.baudRate.Size = new System.Drawing.Size(121, 21);
+            this.baudRate.TabIndex = 10;
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon1.Icon")));
+            this.notifyIcon1.Text = "Serial Hardware Monitor";
+            this.notifyIcon1.Visible = true;
+            this.notifyIcon1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon1_MouseClick);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 140);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(56, 13);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Raw data:";
+            // 
+            // rawData
+            // 
+            this.rawData.AutoSize = true;
+            this.rawData.Location = new System.Drawing.Point(64, 140);
+            this.rawData.Name = "rawData";
+            this.rawData.Size = new System.Drawing.Size(59, 13);
+            this.rawData.TabIndex = 12;
+            this.rawData.Text = "43;10;9;1E";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.Control;
+            this.ClientSize = new System.Drawing.Size(266, 160);
+            this.Controls.Add(this.rawData);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.baudRate);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.interval);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.serialPorts);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Start);
+            this.Controls.Add(this.choosedStats);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Serial Hardware Monitor";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            ((System.ComponentModel.ISupportInitialize)(this.interval)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.CheckedListBox choosedStats;
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox serialPorts;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown interval;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Timer timerSerial;
+        private System.Windows.Forms.ComboBox baudRate;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label rawData;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,285 @@
+﻿//TODO: fix crashing when sending data to other serial ports
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO.Ports;
+using OpenHardwareMonitor.Hardware;
+
+namespace SerialHardwareMonitor
+{
+    [Flags]
+    public enum Stats
+    {
+        CPU_USAGE = 1,
+        CPU_TEMP = 2,
+        GPU_USAGE = 4,
+        GPU_TEMP = 8,
+        RAM_USAGE = 16
+    }
+
+    public partial class Form1 : Form
+    {
+        Computer computer = new Computer()
+        {
+            GPUEnabled = true,
+            CPUEnabled = true,
+            RAMEnabled = true
+        };
+
+        Stats selectedStats;
+        private SerialPort port = new SerialPort();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            InitUI();
+            computer.Open();
+
+        }
+
+       
+        private void InitUI()
+        {
+            //serila port options
+            string[] ports = SerialPort.GetPortNames();
+            foreach (string port in ports)
+            {
+                Console.WriteLine(port);
+
+                serialPorts.Items.Add(port);
+            }
+
+            //Default baud rate
+            baudRate.SelectedItem = "9600";
+
+            // Available sensors
+            foreach (var stat in Enum.GetValues(typeof(Stats)))
+            {
+                choosedStats.Items.Add(stat);
+            }
+
+        }
+
+        private void timerSerial_Tick(object sender, EventArgs e)
+        {
+            Status();
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                //Check input
+                if (serialPorts.SelectedItem == null)
+                    throw new Exception("Selec serial port");
+
+                //Check chosed stats
+                selectedStats = 0;
+                foreach (var stat in choosedStats.CheckedItems)
+                {
+                    selectedStats |= (Stats)stat;
+                }
+                if (selectedStats == 0)
+                    throw new Exception("Choose at least one sensor value!");
+
+                //Open serial port
+                port.PortName = serialPorts.SelectedItem.ToString();
+                port.BaudRate = Convert.ToInt32(baudRate.SelectedItem);
+                port.Open();
+
+                // start timer
+                timerSerial.Enabled = true;
+                timerSerial.Interval = (int)interval.Value;
+
+                // Disable inputs
+                serialPorts.Enabled = false;
+                baudRate.Enabled = false;
+                interval.Enabled = false;
+                Start.Enabled = false;
+                choosedStats.Enabled = false;
+                Stop.Enabled = true;
+
+                // Change icon
+                
+                notifyIcon1.Icon = (Icon)Properties.Resources.ResourceManager.GetObject("icon green.ico");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+            // Close port
+            try
+            {
+                //port.Write("DIS*");
+                port.Close();
+
+                //Stop timer
+                timerSerial.Enabled = false;
+
+                // Enable inputs
+                serialPorts.Enabled = true;
+                baudRate.Enabled = true;
+                interval.Enabled = true;
+                Start.Enabled = true;
+                choosedStats.Enabled = true;
+                Stop.Enabled = false;
+
+                //clear raw data
+                rawData.Text = "";
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void Status()
+        {
+            List<string> values = new List<string>();
+
+            if (selectedStats.HasFlag(Stats.CPU_USAGE))
+            {
+                int value = (int)GetSensorsValue(HardwareType.CPU, SensorType.Load, "CPU Total");
+                values.Add(value.ToString());
+            }
+
+            if (selectedStats.HasFlag(Stats.CPU_TEMP))
+            {
+                int value = (int)GetAvgTemp(HardwareType.CPU);
+                values.Add(value.ToString());
+            }
+
+            if (selectedStats.HasFlag(Stats.GPU_USAGE))
+            {
+                //One of them will return 0;
+                int amd = (int)GetSensorsValue(HardwareType.GpuAti, SensorType.Load, "GPU Core");
+                int nvidia = (int)GetSensorsValue(HardwareType.GpuNvidia, SensorType.Load, "GPU Core");
+
+                values.Add(Math.Max(amd, nvidia).ToString());
+            }
+
+            if (selectedStats.HasFlag(Stats.GPU_TEMP))
+            {
+                //One of them will return 0;
+                int amd = (int)GetSensorsValue(HardwareType.GpuAti, SensorType.Temperature, "GPU Core");
+                int nvidia = (int)GetSensorsValue(HardwareType.GpuNvidia, SensorType.Temperature, "GPU Core");
+
+                values.Add(Math.Max(amd, nvidia).ToString());
+            }
+            if (selectedStats.HasFlag(Stats.RAM_USAGE))
+            {
+                int value = (int)GetSensorsValue(HardwareType.RAM, SensorType.Load, "Memory");
+                values.Add(value.ToString());
+
+            }
+
+            try
+            {
+                // Put ; between each value and E to the end
+                string message = string.Join(";", values.ToArray()) + "E";
+                byte[] data = Encoding.ASCII.GetBytes(message);
+                port.Write(data, 0, data.Length);
+
+                //display raw data
+                rawData.Text = message;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            }
+        }
+        private float GetSensorsValue(HardwareType type, SensorType sensorType, string Name)
+        {
+            // Find hardware
+            IHardware hardware = computer.Hardware.Where(device => device.HardwareType == type).FirstOrDefault();
+            if (hardware == null)
+                return 0.0f;
+
+            hardware.Update();
+
+            // Find needes sensor
+            ISensor sensor = hardware.Sensors.Where(sensor1 => sensor1.SensorType == sensorType && sensor1.Name == Name).FirstOrDefault();
+            if (sensor == null)
+                return 0.0f;
+            return sensor.Value.GetValueOrDefault();
+        }
+
+        private float GetAvgTemp(HardwareType type)
+        {
+            var gpus = computer.Hardware.Where(x => x.HardwareType == type).ToArray();
+            if (gpus.Any())
+            {
+                int n = 0;
+                float t = 0;
+                foreach (var gpu in gpus)
+                {
+                    var temps = gpu.Sensors.Where(x => x.SensorType == SensorType.Temperature).ToArray();
+                    if (temps.Any())
+                    {
+                        var temp = temps.Average(x => x.Value.Value);
+                        t += temp;
+                        n++;
+                    }
+
+                    foreach (var sh in gpu.SubHardware)
+                    {
+                        temps = sh.Sensors.Where(x => x.SensorType == SensorType.Temperature).ToArray();
+                        if (temps.Any())
+                        {
+                            var temp = temps.Average(x => x.Value.Value);
+                            t += temp;
+                            n++;
+                        }
+                    }
+                }
+
+                if (n > 0)
+                {
+                    return t / (float)n;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+        ~Form1()
+        {
+            port.Close();
+            port.Dispose();
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Hide();
+            }
+        }
+
+        private void notifyIcon1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                this.Show();
+                this.WindowState = FormWindowState.Normal;
+            }
+        }
+    }
+}
